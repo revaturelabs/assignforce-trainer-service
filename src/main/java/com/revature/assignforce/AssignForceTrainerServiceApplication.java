df7534fbbde2f1aafbbe24583864e92aa204751f
tjansen17 <tnj@17cosmicln.net>
2018-07-20 11:34:27 -0400

    Pom updated, startup changed.

--- src/main/java/com/revature/assignforce/AssignForceTrainerServiceApplication.java
+++ src/main/java/com/revature/assignforce/AssignForceTrainerServiceApplication.java
@@ -2,6 +2,7 @@ package com.revature.assignforce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
@@ -9,6 +10,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class AssignForceTrainerServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AssignForceTrainerServiceApplication.class, args);
+		new SpringApplicationBuilder(AssignForceTrainerServiceApplication.class).run(args);
+
+//		SpringApplication.run(AssignForceTrainerServiceApplication.class, args);
 	}
 }
