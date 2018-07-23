4e66bf844bd731a16f70adde1048cd740f22f1c4
Rodrigo Espinosa <rod_elmariachi@Rodrigos-MacBook-Pro.local>
2018-07-23 19:47:18 -0400

    Added testing annotations to CertTest.java

--- src/test/java/com/revature/tests/CertTest.java
+++ src/test/java/com/revature/tests/CertTest.java
@@ -3,12 +3,17 @@ package com.revature.tests;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.revature.assignforce.beans.Cert;
 import com.revature.assignforce.beans.SkillIdHolder;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
 public class CertTest {
 
 	@Configuration
