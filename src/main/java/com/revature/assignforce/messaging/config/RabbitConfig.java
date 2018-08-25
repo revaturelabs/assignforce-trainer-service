3974546465611742a03e32746586fd0d57b9936e
August Duet <august.duet@revature.com>
2018-08-25 10:30:33 -0400

    rabbit

--- src/main/java/com/revature/assignforce/messaging/config/RabbitConfig.java
+++ src/main/java/com/revature/assignforce/messaging/config/RabbitConfig.java
@@ -1,5 +1,6 @@
 package com.revature.assignforce.messaging.config;
 
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,9 +12,6 @@ import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 @Configuration
 public class RabbitConfig {
 
-	@Value("${VCAP_SERVICES:{}}")
-	String uri;
-	
 	Logger log = Logger.getLogger(RabbitConfig.class);
 
 	@Bean
@@ -24,7 +22,6 @@ public class RabbitConfig {
 			if(ack) log.debug("Message confirmed");
 			else log.error("Confirmation failure");
 		});
-		System.out.println(uri);
 		return rabbitTemplate;
 	}
 }
