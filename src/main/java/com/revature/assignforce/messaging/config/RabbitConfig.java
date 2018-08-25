f17576ca9a228087f4ae42de048ca66390eb2406
August Duet <august.duet@revature.com>
2018-08-25 13:09:50 -0400

    rabbit

--- src/main/java/com/revature/assignforce/messaging/config/RabbitConfig.java
+++ src/main/java/com/revature/assignforce/messaging/config/RabbitConfig.java
@@ -1,13 +1,18 @@
 package com.revature.assignforce.messaging.config;
 
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.RabbitConnectionFactoryBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.service.messaging.RabbitConnectionFactoryConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.jboss.logging.Logger;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+
+import java.util.Properties;
 
 @Configuration
 public class RabbitConfig {
