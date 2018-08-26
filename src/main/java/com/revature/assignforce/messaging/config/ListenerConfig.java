19f9e507b65fe7e1f61addd73e0c5e118b459359
August Duet <august.duet@revature.com>
2018-08-26 00:37:40 -0400

    configuring listeners

--- src/main/java/com/revature/assignforce/messaging/config/ListenerConfig.java
+++ src/main/java/com/revature/assignforce/messaging/config/ListenerConfig.java
@@ -26,6 +26,5 @@ public class ListenerConfig implements RabbitListenerConfigurer {
 	@Override
 	public void configureRabbitListeners(RabbitListenerEndpointRegistrar registrar) {
 		registrar.setMessageHandlerMethodFactory(messageHandlerMethodFactory());
-		regist
 	}
 }
