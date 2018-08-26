5ba3f37db71348c84c70a30e1d286bda33b3097f
August Duet <august.duet@revature.com>
2018-08-25 22:58:51 -0400

    updating message processing for skills

--- src/main/java/com/revature/assignforce/messaging/listeners/SkillListener.java
+++ src/main/java/com/revature/assignforce/messaging/listeners/SkillListener.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.QueueBinding;
@@ -19,32 +21,46 @@ import com.revature.assignforce.service.TrainerService;
 
 @Component
 public class SkillListener {
+
+	Logger logger = LoggerFactory.getLogger(SkillListener.class);
+
 	private TrainerService trainerService;
 	private final String trainerQueue;
-	
-	public SkillListener(TrainerService trainerService, @Value("${spring.rabbitmq.batch-queue:trainer-queue}") String trainerQueue) {
+
+	public SkillListener(TrainerService trainerService,
+			@Value("${spring.rabbitmq.batch-queue:trainer-queue}") String trainerQueue) {
 		super();
 		this.trainerService = trainerService;
 		this.trainerQueue = trainerQueue;
 	}
-	
-	@RabbitListener(bindings = @QueueBinding(
-			value = @Queue(value = "trainer-queue", durable = "true"),
-			exchange = @Exchange(value = "assignforce", ignoreDeclarationExceptions = "true"),
-			key = "assignforce.skill.delete"))
-	public void receiveMessage(final Integer skillId, Channel channel, 
-			@Header(AmqpHeaders.DELIVERY_TAG) long tag) {
-		
+
+	@RabbitListener(bindings = @QueueBinding(value = @Queue(value = "trainer-queue", durable = "true"), exchange = @Exchange(value = "assignforce", ignoreDeclarationExceptions = "true"), key = "assignforce.skill.delete"))
+	public void receiveMessage(final Integer skillId, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long tag) {
+
 		try {
 			channel.basicAck(tag, false);
-		} catch(IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		List<Trainer> trainers = trainerService.getAll();
-		
-		trainers.forEach(trainer -> trainer.getSkills().removeIf(skillIdHolder -> skillIdHolder.getSkillId() == skillId));
-		
+
+		trainers.forEach(
+				trainer -> trainer.getSkills().removeIf(skillIdHolder -> skillIdHolder.getSkillId() == skillId));
+
 		trainers.forEach(trainer -> trainerService.update(trainer));
+	}
+
+	@RabbitListener(bindings = @QueueBinding(value = @Queue(value = "trainer-queue", durable = "true"), exchange = @Exchange(value = "assignforce", ignoreDeclarationExceptions = "true"), key = "assignforce.skill.deactivate"))
+	public void receiveDeactivateMessage(final Integer skillId, Channel channel,
+			@Header(AmqpHeaders.DELIVERY_TAG) long tag) {
+
+		try {
+			logger.info("Deactivation message received for skill " + skillId);
+			channel.basicAck(tag, false);
+		} catch (IOException e) {
+			logger.warn("Exception will processing deactivation message for skill " + skillId);
+			logger.warn(e.getMessage());
+		}
 	}
 }
