006ff5c182c8d3b896797583a908caf737d459af
August Duet <august.duet@revature.com>
2019-03-03 01:44:56 -0500

    remove old pcf deps

--- src/main/java/com/revature/assignforce/AssignForceTrainerServiceApplication.java
+++ src/main/java/com/revature/assignforce/AssignForceTrainerServiceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients
 @EnableCircuitBreaker
