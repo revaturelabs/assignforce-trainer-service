e3dd4c0e57168418a62b3ff21ec4adff71af7009
August Duet <august.duet@revature.com>
2019-03-07 12:11:29 -0500

    adding messaging

--- src/main/java/com/revature/assignforce/config/BeansConfig.java
+++ src/main/java/com/revature/assignforce/config/BeansConfig.java
@@ -3,6 +3,7 @@ package com.revature.assignforce.config;
 import com.revature.assignforce.service.TrainerService;
 import com.revature.assignforce.service.TrainerServiceImpl;
 import org.springframework.cloud.aws.messaging.config.QueueMessageHandlerFactory;
+import org.springframework.cloud.aws.messaging.support.NotificationMessageArgumentResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
@@ -25,7 +26,7 @@ public class BeansConfig {
 
         //set strict content type match to false
         messageConverter.setStrictContentTypeMatch(false);
-        factory.setArgumentResolvers(Collections.<HandlerMethodArgumentResolver>singletonList(new PayloadArgumentResolver(messageConverter)));
+        factory.setArgumentResolvers(Collections.<HandlerMethodArgumentResolver>singletonList(new NotificationMessageArgumentResolver(messageConverter)));
         return factory;
     }
 }
