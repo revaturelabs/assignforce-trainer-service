3e8d11cf767904b503cd89ff7e50623f535a9baa
August Duet <august.duet@revature.com>
2019-03-07 11:23:05 -0500

    adding messaging

--- src/main/java/com/revature/assignforce/AssignForceTrainerServiceApplication.java
+++ src/main/java/com/revature/assignforce/AssignForceTrainerServiceApplication.java
@@ -2,6 +2,7 @@ package com.revature.assignforce;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.aws.messaging.config.annotation.EnableSqs;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -11,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @EnableFeignClients
 @EnableCircuitBreaker
 @EnableWebSecurity
+@EnableSqs
 public class AssignForceTrainerServiceApplication {
 
 	public static void main(String[] args) {
