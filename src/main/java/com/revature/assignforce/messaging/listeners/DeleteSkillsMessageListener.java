5940adf35f4b76cfb9e26940293c736d384596bb
August Duet <august.duet@revature.com>
2019-03-07 11:20:27 -0500

    adding messaging

--- src/main/java/com/revature/assignforce/messaging/listeners/DeleteSkillsMessageListener.java
+++ src/main/java/com/revature/assignforce/messaging/listeners/DeleteSkillsMessageListener.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.aws.messaging.config.annotation.NotificationMessage;
+import org.springframework.cloud.aws.messaging.listener.SqsMessageDeletionPolicy;
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
 import org.springframework.stereotype.Component;
 
@@ -21,7 +22,7 @@ public class DeleteSkillsMessageListener implements SkillsMessageListener {
     }
 
     @Override
-    @SqsListener("${app.sqs.queues.del-skill-queue}")
+    @SqsListener(value="${app.sqs.queues.del-skill-queue}", deletionPolicy= SqsMessageDeletionPolicy.ON_SUCCESS)
     public void receive(final @NotificationMessage SkillIdHolder skillMessage) {
         LOG.info("Delete skill id reference -- " + skillMessage.getSkillId());
         skillRepository.delete(skillMessage);
