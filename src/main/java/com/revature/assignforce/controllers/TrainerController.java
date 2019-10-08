e97eb74bb06245bccaaacdc260ecf7fdd99f4605
Gabriel Ferrer <gabrielferrergtf@gmail.com>
2019-10-08 13:12:43 -0400

    final

--- src/main/java/com/revature/assignforce/controllers/TrainerController.java
+++ src/main/java/com/revature/assignforce/controllers/TrainerController.java
@@ -18,12 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
 import com.revature.assignforce.beans.Trainer;
 import com.revature.assignforce.service.TrainerService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 /**
  * 
  * A controller for retrieving, creating, updating and deleting Trainer information
  *
  */
 @RestController
+@Api(value = "CertController")
 public class TrainerController {
 
 	@Autowired
@@ -34,6 +41,9 @@ public class TrainerController {
 	 * @return		A List of All Trainers
 	 */
 	@GetMapping
+	@ApiOperation(value = "Get All Trainers", 
+	response = Trainer.class, responseContainer="List", 
+	tags = "TrainerController", nickname= "getAllTrainers")
 	public List<Trainer> getAll() {
 		return trainerService.getAll();
 	}
@@ -43,10 +53,16 @@ public class TrainerController {
 	 * @param 	id	A Trainer Id of object to be retrieved
 	 * @return		A Trainer ResponseEntity
 	 * @see		Trainer
-	 * @see		ResponseEntity
+	 *
 	 */
 	@GetMapping(value = "{id}")
-	public ResponseEntity<Trainer> getById(@PathVariable int id) {
+	@ApiOperation(value = "Get Trainer Information by Id", 
+	response = ResponseEntity.class, 
+	tags = "TrainerController", nickname= "getTrainerById")
+	@ApiResponses(value = {
+			@ApiResponse(code = 404, message = "Not Found"), 
+            @ApiResponse(code = 200, message = "OK", response = Trainer.class)}) 
+	public ResponseEntity<Trainer> getById(@ApiParam(name = "id") @PathVariable int id) {
 		Optional<Trainer> t = trainerService.findById(id);
 		if (!t.isPresent())
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -58,10 +74,17 @@ public class TrainerController {
 	 * @param 	email	The email of the trainer object to be retrieved
 	 * @return			A Trainer ResponseEntity
 	 * @see		Trainer
-	 * @see		ResponseEntity
+	 *
 	 */
 	@GetMapping(value = "email/{email}")
-	public ResponseEntity<Trainer> getByEmail(@PathVariable String email) {
+	@ApiOperation(value = "Get Trainer Information by Email", 
+	response = ResponseEntity.class, 
+	tags = "TrainerController", nickname= "getTrainerByEmail")
+	@ApiResponses(value = {
+			@ApiResponse(code = 404, message = "Not Found"), 
+            @ApiResponse(code = 200, message = "OK", 
+            response = Trainer.class)}) 
+	public ResponseEntity<Trainer> getByEmail(@ApiParam(name = "email") @PathVariable String email) {
 		Optional<Trainer> t = trainerService.findByEmail(email);
 		if (!t.isPresent())
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -73,10 +96,16 @@ public class TrainerController {
 	 * @param 	skill_id	The skill_id of the trainer object to be retrieved
 	 * @return				A Trainer ResponseEntity
 	 * @see		Trainer
-	 * @see		ResponseEntity
+	 *
 	 */
 	@GetMapping(value = "skill/{skill_id}")
-	public ResponseEntity<List<Trainer>> getBySkill (@PathVariable int skill_id) {
+	@ApiOperation(value = "Get All Trainers with given Skill Id",  
+	tags = "TrainerController", nickname= "getTrainersBySkillId")
+	@ApiResponses(value = {
+			@ApiResponse(code = 404, message = "Not Found"), 
+            @ApiResponse(code = 200, message = "OK",
+            response = Trainer.class, responseContainer="List")}) 
+	public ResponseEntity<List<Trainer>> getBySkill (@ApiParam(name = "skill_id") @PathVariable int skill_id) {
 		List<Trainer> trainers = trainerService.findBySkills(skill_id);
 		if (trainers == null || trainers.isEmpty()){
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -89,10 +118,16 @@ public class TrainerController {
 	 * @param 	lastName	The lastName of the trainer object to be retrieved
 	 * @return				A Trainer ResponseEntity
 	 * @see		Trainer
-	 * @see		ResponseEntity
+	 *
 	 */
 	@GetMapping(value = "lastName/{lastName}")
-	public ResponseEntity<List<Trainer>> getByLastName (@PathVariable String lastName) {
+	@ApiOperation(value = "Get All Trainers with a given Lastname", 
+	tags = "TrainerController", nickname= "getTrainersByLastname")
+	@ApiResponses(value = {
+			@ApiResponse(code = 404, message = "Not Found"), 
+            @ApiResponse(code = 200, message = "OK", 
+            response = Trainer.class, responseContainer="List")}) 
+	public ResponseEntity<List<Trainer>> getByLastName (@ApiParam(name = "lastName") @PathVariable String lastName) {
 		List<Trainer> trainers = trainerService.findByLastName(lastName);
 		if (trainers == null || trainers.isEmpty()) {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -106,11 +141,17 @@ public class TrainerController {
 	 * @param 	lastName	The lastName of the trainer object to be retrieved
 	 * @return				A Trainer ResponseEntity
 	 * @see		Trainer
-	 * @see		ResponseEntity
+	 *
 	 */
 	@GetMapping(value = "firstAndLast/{firstName}/{lastName}")
-	public ResponseEntity<List<Trainer>> getByFirstAndLastName (@PathVariable String firstName,
-														  @PathVariable String lastName) {
+	@ApiOperation(value = "Get All Trainers with given Firstname and Lastname",
+	tags = "TrainerController", nickname= "getTrainersByFirstnameAndLastname")
+	@ApiResponses(value = {
+			@ApiResponse(code = 404, message = "Not Found"), 
+            @ApiResponse(code = 200, message = "OK", 
+            response = Trainer.class, responseContainer="List")}) 
+	public ResponseEntity<List<Trainer>> getByFirstAndLastName (@ApiParam(name = "firstName") @PathVariable String firstName,
+			@ApiParam(name = "lastName") @PathVariable String lastName) {
 		List<Trainer> trainers = trainerService.findByFirstAndLastName(firstName, lastName);
 		if (trainers == null || trainers.isEmpty()) {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -123,10 +164,17 @@ public class TrainerController {
 	 * @param 	firstName	The firstName of the trainer object to be retrieved
 	 * @return				A Trainer ResponseEntity
 	 * @see		Trainer
-	 * @see		ResponseEntity
+	 *
 	 */
 	@GetMapping(value = "firstName/{firstName}")
-	public ResponseEntity<List<Trainer>> getByFirstName (@PathVariable String firstName) {
+	@ApiOperation(value = "Get All Trainers with a given Firstname", 
+	//response = ResponseEntity.class, 
+	tags = "TrainerController", nickname= "getTrainersByFirstname")
+	@ApiResponses(value = {
+			@ApiResponse(code = 404, message = "Not Found"), 
+            @ApiResponse(code = 200, message = "OK", 
+            response = Trainer.class, responseContainer="List")}) 
+	public ResponseEntity<List<Trainer>> getByFirstName (@ApiParam(name = "firstName") @PathVariable String firstName) {
 		List<Trainer> trainers = trainerService.findByFirstName(firstName);
 		if (trainers == null || trainers.isEmpty()) {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -139,9 +187,15 @@ public class TrainerController {
 	 * @param 	t	A New Trainer object
 	 * @return		A Trainer ResponseEntity
 	 * @see		Trainer
-	 * @see		ResponseEntity
+	 *
 	 */
 	@PostMapping
+	@ApiOperation(value = "Create New Trainer Information", 
+	response = ResponseEntity.class, 
+	tags = "TrainerController", nickname= "addTrainer")
+	@ApiResponses(value = {
+			@ApiResponse(code = 400, message = "Bad Request"), 
+            @ApiResponse(code = 201, message = "Created", response = Trainer.class)})
 	public ResponseEntity<Trainer> add(@RequestBody Trainer t) {
 		t = trainerService.create(t);
 		if (t == null)
@@ -154,9 +208,15 @@ public class TrainerController {
 	 * @param 	t	An Edited Trainer object
 	 * @return		A Trainer ResponseEntity
 	 * @see		Trainer
-	 * @see		ResponseEntity
+	 *
 	 */
-	@PutMapping("{id}")
+	//@PutMapping("{id}")
+	@ApiOperation(value = "Update Trainer Information", 
+	response = ResponseEntity.class, 
+	tags = "TrainerController", nickname= "updateTrainer")
+	@ApiResponses(value = {
+			@ApiResponse(code = 400, message = "Bad Request"), 
+            @ApiResponse(code = 201, message = "Created", response = Trainer.class)}) 
 	public ResponseEntity<Trainer> update(@RequestBody Trainer t) {
 		t = trainerService.update(t);
 		if (t == null)
@@ -169,10 +229,16 @@ public class TrainerController {
 	 * @param 	id	A Trainer Id of object to be deleted
 	 * @return		A Trainer ResponseEntity
 	 * @see		Trainer
-	 * @see		ResponseEntity
+	 *
 	 */
 	@DeleteMapping(value = "{id}")
-	public ResponseEntity<Trainer> delete(@PathVariable int id) {
+	@ApiOperation(value = "Delete Trainer Information", 
+	response = ResponseEntity.class, 
+	tags = "TrainerController", nickname= "deleteTrainer")
+	@ApiResponses(value = {
+			@ApiResponse(code = 404, message = "Not Found"), 
+            @ApiResponse(code = 200, message = "OK", response = Trainer.class)}) 
+	public ResponseEntity<Trainer> delete(@ApiParam(name = "id") @PathVariable int id) {
 		if (trainerService.findById(id) == null)
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		trainerService.delete(id);
